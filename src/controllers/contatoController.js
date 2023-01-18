6314fb0c9452ed2753b93d19aa256dea8fc36be4
talesmilan <100313945+talesmilan@users.noreply.github.com>
2023-01-18 13:50:54 -0300

    Editando e excluindo contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -33,3 +33,34 @@ exports.editIndex = async (req, res) => {
     res.render('contato', { contato })
 }
 
+exports.edit = async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404')
+        const contato = new Contato(req.body)
+        await contato.edit(req.params.id)
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors)
+            req.session.save(() => res.redirect(`../index/${req.params.id}`))
+            return
+        }
+        req.flash('success', "Contato editado com sucesso.")
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`))
+        return
+    } catch(e) {
+        console.log(e)
+        res.render('404')
+    }
+}
+
+exports.delete = async function(req,res) {
+    if (!req.params.id) return res.render('404')
+
+    const contato = await Contato.delete(req.params.id)
+
+    if(!contato) return res.render('404')
+
+    req.flash('success', "Seu contato foi deletado com sucesso.")
+    req.session.save(() => res.redirect(`/`))
+    return
+
+}
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,12 +17,6 @@ function Contato(body) {
     this.contato = null
 }
 
-Contato.buscaPorId = async function(id) {
-    if (typeof id !== 'string') return
-    const user = await ContatoModel.findById(id)
-    return user
-}
-
 Contato.prototype.register = async function() {
     this.valida()
     if (this.errors.length > 0) return
@@ -56,6 +50,34 @@ Contato.prototype.cleanUp = function() {
       telefone: this.body.telefone
     };
   }
+
+
+Contato.prototype.edit = async function(id) {
+  if (typeof id !== 'string') return
+    this.valida()
+    if (this.errors.length > 0) return
+    this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true})
+  
+}
+// Métodos estáticos
+Contato.buscaPorId = async function(id) {
+  if (typeof id !== 'string') return
+  const contato = await ContatoModel.findById(id)
+  return contato
+}
+
+Contato.buscaContatos = async function() {
+  const contatos = await ContatoModel.find()
+    .sort({data: -1})
+  return contatos
+}
+
+Contato.delete = async function(id) {
+  if (typeof id !== 'string') return
+  const contato = await ContatoModel.findOneAndDelete({_id: id})
+  return contato
+}
+
 
 
 module.exports = Contato
